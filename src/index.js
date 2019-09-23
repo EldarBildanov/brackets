b9b058ce1ede5aeaae6f02f35225b5d98b1e6dbd
EldarBildanov <eldar.bildanov@gmail.com>
2019-09-24 00:28:57 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 == 0){
+    let mask;
+    let in_length = str.length;
+    for (let k = 0; k  < in_length; k++){
+      for (let i = 0; i < bracketsConfig.length; i++){
+      mask = bracketsConfig[i][0] + bracketsConfig[i][1];
+
+      while (str.indexOf(mask,0) != -1 ){
+        str = str.slice(0,str.indexOf(mask,0)) + str.slice(str.indexOf(mask,0)+2);
+      }    
+    }
+    
+    if (str.length == 0) return true;
+    } 
+  }
+  return false;
 }
